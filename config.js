cdf5dce68fb0c1324d4cd8f1778d874ded6ce355
serkankarakad-gif <serkankarakad@gmail.com>
2026-06-14 16:21:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB5pl78DRao2SmUWsMYMSZ6YbfX4rtRNdc",
+  authDomain: "gamezone-e11b0.firebaseapp.com",
+  databaseURL: "https://gamezone-e11b0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gamezone-e11b0",
+  storageBucket: "gamezone-e11b0.firebasestorage.app",
+  messagingSenderId: "775694460272",
+  appId: "1:775694460272:web:7e5fd5691df9d8399d5bb5",
+  measurementId: "G-3M7FXX8XR4"
+};
